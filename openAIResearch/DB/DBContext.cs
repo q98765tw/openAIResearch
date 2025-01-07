45d5fd53347640414a8e3de62809977102370dae
q98765tw <q98765tw@gmail.com>
2025-01-07 17:36:48 +0800

    feat : 想加postgres，加得很奇怪

---
+++ openAIResearch/DB/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    // 定义你的表，例如：
+    public DbSet<User> Users { get; set; }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- openAIResearch/Program.cs
+++ openAIResearch/Program.cs
@@ -1,15 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// 添加数据库上下文
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// �[�J�]�w��
+// 加入設定檔
 builder.Configuration.AddJsonFile("appsettings.json");
 var app = builder.Build();
-
+app.MapGet("/", async (AppDbContext db) =>
+{
+    var users = await db.Users.ToListAsync();
+    return users;
+});
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
